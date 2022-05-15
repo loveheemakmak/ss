a4699226ada5adf5891db1b7047a2a97f086cfa4
Love Hee Mak Mak <78906291+loveheemakmak@users.noreply.github.com>
2022-05-15 15:44:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <iframe style="width:100%; height:100%; left=0; top=0; right=0; bottom=0;" src="https://www.youtube.com/embed/NqaQC5RnDds?autoplay=1&playlist=NqaQC5RnDds&loop=1" title="บิดอ้ายทำไม" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe style="width:100%; height:700px; left=0; top=0; right=0; bottom=0;" src="https://www.youtube.com/embed/NqaQC5RnDds?autoplay=1&playlist=NqaQC5RnDds&loop=1" title="บิดอ้ายทำไม" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </body>
 </html>
